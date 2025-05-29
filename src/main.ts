ce8a6e47e85197fce967ca1083c393eaa00c887b
Ayush Soni <soni.ayush.2212@gmail.com>
2025-05-29 22:48:45 +0530

    Change the color of the inputs after clicking the buttons

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import "./style.css";
+
+let selectedColor = "";
+let selectedTheme = "";
+
+const colorButtons =
+  document.querySelectorAll<HTMLButtonElement>(".btn-picker");
+const uploadButton = document.querySelector<HTMLDivElement>(".file-label");
+
+colorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Get the background color of the button
+
+    const backgroundColor = getComputedStyle(button).backgroundColor;
+
+    uploadButton?.style.setProperty("background-color", backgroundColor);
+  });
+});
