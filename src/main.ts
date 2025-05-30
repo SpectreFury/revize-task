4807b69cc08552e9317921ea3ce7752adc19898a
Ayush Soni <soni.ayush.2212@gmail.com>
2025-05-30 15:46:38 +0530

    Add loader in place of umbrella when the file is uploaded

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import "./style.css";
-
 const colorMap: Record<string, string> = {
   "rgb(55, 184, 230)": "var(--blue-bg)",
   "rgb(217, 54, 141)": "var(--pink-bg)",
@@ -18,12 +16,13 @@ const uploadButton = document.querySelector<HTMLDivElement>(".file-label");
 const body = document.querySelector<HTMLBodyElement>("body");
 const umbrellaImg = document.querySelector<HTMLImageElement>(".umbrella-image");
 const logoImg = document.querySelector<HTMLImageElement>(".logo-icon");
-
 const input = document.getElementById("file-input") as HTMLInputElement;
 const inputText = document.querySelector<HTMLSpanElement>(".input-text");
-
 const uploadImg = document.querySelector<HTMLImageElement>(".upload-icon");
 const loaderImg = document.querySelector<HTMLImageElement>(".loader-icon");
+const loaderContainer =
+  document.querySelector<HTMLDivElement>(".loader-container");
+const loaderPath = document.getElementById("loader-path");
 
 colorButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -39,16 +38,38 @@ input.addEventListener("change", () => {
   if (!file || !inputText) return;
 
   // Simulate a loading state
-
   loaderImg?.style.setProperty("display", "inline-block");
   uploadImg?.style.setProperty("display", "none");
 
   setTimeout(() => {
     inputText.textContent = file.name;
-    logoImg?.setAttribute("src", "Revize.avif");
 
     loaderImg?.style.setProperty("display", "none");
     uploadImg?.style.setProperty("display", "inline-block");
+
+    // Hide umbrella and show loader
+
+    umbrellaImg?.style.setProperty("display", "none");
+    loaderContainer?.style.setProperty("display", "flex");
+    loaderPath?.setAttribute("fill", "rgb(55, 184, 230)");
+    logoImg?.setAttribute("src", "");
+
+    setTimeout(() => {
+      // Show the umbrella and hide loader
+      umbrellaImg?.style.setProperty("display", "flex");
+      loaderContainer?.style.setProperty("display", "none");
+
+      loaderPath?.setAttribute("fill", "rgb(55, 184, 230)");
+
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        if (logoImg) {
+          logoImg.setAttribute("src", fileReader.result as string);
+        }
+      };
+    }, 2000);
   }, 1000);
 });
 
